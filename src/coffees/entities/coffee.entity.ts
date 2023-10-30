8c3ac1ad44fb4c999b64bf4cec28ea88bf17de7a
LambdaC <studycwg@gmail.com>
2023-10-30 23:01:37 +0800

    create a typeorm entity

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,7 +1,19 @@
 /* eslint-disable prettier/prettier */
+
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('coffees')
 export class Coffee {
+
+    @PrimaryGeneratedColumn()
     id: number;
+
+    @Column()
     name: string;
+
+    @Column()
     brand: string;
+
+    @Column('json', { nullable: true })
     flavors: string[];
 }
